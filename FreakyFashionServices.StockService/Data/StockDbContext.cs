a558b9687ecc094a9f26128eab4b97b509cf28e9
Your NameMousaAzm <mousa.azimi77@gmail.com>
2022-01-04 15:39:03 +0100

    Add dbContext /Stock

---
+++ FreakyFashionServices.StockService/Data/StockDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FreakyFashionServices.StockService.Models.DbModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreakyFashionServices.StockService.Data
+{
+    public class StockDbContext : DbContext
+    {
+        public StockDbContext(DbContextOptions<StockDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Stock> Stocks { get; set; } = null!;
+    }
+}
--- FreakyFashionServices.StockService/Program.cs
+++ FreakyFashionServices.StockService/Program.cs
@@ -1,3 +1,5 @@
+using FreakyFashionServices.StockService.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<StockDbContext>(
+   options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
 
 var app = builder.Build();
 
