e018566373b15dfe1573949b00a28bf9ca9cab5a
Your NameMousaAzm <mousa.azimi77@gmail.com>
2022-01-04 14:40:34 +0100

    Add context /Basket

---
+++ FreakyFashionServices.BasketService/Data/BasketDbContext.cs
@@ -0,0 +1,18 @@
+﻿using FreakyFashionServices.BasketService.Models.DbModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreakyFashionServices.BasketService.Data
+{
+    
+    public class BasketDbContext : DbContext
+    {
+
+        public BasketDbContext(DbContextOptions<BasketDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; } = null!;
+
+    }
+}
