55376447e02263a39be20767d6b8003b4203acb2
Your NameMousaAzm <mousa.azimi77@gmail.com>
2022-01-04 15:29:29 +0100

    Add order controller /Order

---
+++ FreakyFashionServices.OrderService/Controllers/OrderController.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using FreakyFashionServices.OrderService.Models.DbModel;
+using FreakyFashionServices.OrderService.Models.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace FreakyFashionServices.OrderService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+       
+        private readonly IMapper _mapper;
+        public OrderController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        //[HttpGet]
+        //public IActionResult GetOrders()
+        //{
+        //    var getOrder = _context.Orders.ToList();
+        //    if(getOrder == null) return NotFound();
+        //    return Ok(_mapper.Map<IEnumerable<ReadOrderDto>>(getOrder));
+        //}
+
+        [HttpPost]
+        public IActionResult CreateOrder([FromBody] CreateOrderDto createOrderDto)
+        {
+           return NoContent();  
+          
+        }
+
+        //[HttpPut("{identifier}")]
+        //public IActionResult UpdateOrder(string identifier, [FromBody] UpdateOrderDto updateOrderDto)
+        //{
+        //    var getOrder = _context.Orders.FirstOrDefault(o => o.Identifier == identifier);
+        //    if (getOrder == null) return NotFound();
+
+        //    _mapper.Map(updateOrderDto, getOrder);
+        //    _context.Orders.Update(getOrder);
+        //    _context.SaveChanges();
+
+        //    return NoContent();
+        //}
+
+        //[HttpDelete("{identifier}")]
+        //public IActionResult DeleteOrder(string identifier)
+        //{
+        //    var getOrder = _context.Orders.FirstOrDefault(o => o.Identifier == identifier);
+        //    if(getOrder == null) return NotFound();
+
+        //    _context.Orders.Remove(getOrder);
+        //    _context.SaveChanges();
+
+        //    return NoContent();
+        //}
+    }
+}
---
+++ FreakyFashionServices.OrderService/Mapping/OrderProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using FreakyFashionServices.OrderService.Models.DbModel;
+using FreakyFashionServices.OrderService.Models.Dtos;
+
+namespace FreakyFashionServices.OrderService.Mapping
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<Order, ReadOrderDto>();
+            CreateMap<CreateOrderDto, Order>();
+            CreateMap<UpdateOrderDto, Order>();
+        }
+    }
+}
